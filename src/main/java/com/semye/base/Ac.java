c431fedc1edbf907ff25418674ea13a9d5e82b23
semye <464622832@qq.com>
2021-03-04 15:19:18 +0800

    commit

--- src/main/java/com/semye/base/Ac.java
+++ src/main/java/com/semye/base/Ac.java
@@ -8,20 +8,20 @@ import java.util.Arrays;
 class Ac {
 
 
-//    public static void main(String[] args) {
-//        int[] a = {2, 44, 5, 23, 56, 78, 9};
-//        int temp;
-//        for (int i = 0; i < a.length - 1; i++) {
-//            for (int j = 0; j < a.length - 1 - i; j++) {
-//                if (a[j] > a[j + 1]) {
-//                    temp = a[j];
-//                    a[j] = a[j + 1];
-//                    a[j + 1] = temp;
-//                }
-//            }
-//        }
-//        System.out.println(Arrays.toString(a));
-//    }
+    public static void main(String[] args) {
+        int[] a = {2, 44, 5, 23, 56, 78, 9};
+        int temp;
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = 0; j < a.length - 1 - i; j++) {
+                if (a[j] > a[j + 1]) {
+                    temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(a));
+    }
 
 
 }
