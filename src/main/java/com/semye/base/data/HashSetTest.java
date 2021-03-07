951e268111e45ab9e8011094df63fbe4c38ebce9
semye <464622832@qq.com>
2021-03-07 22:06:09 +0800

    commit

--- src/main/java/com/semye/base/data/HashSetTest.java
+++ src/main/java/com/semye/base/data/HashSetTest.java
@@ -1,5 +1,6 @@
 package com.semye.base.data;
 
+import java.lang.management.ManagementFactory;
 import java.util.HashSet;
 
 /**
@@ -10,6 +11,11 @@ import java.util.HashSet;
 public class HashSetTest {
 
     public static void main(String[] args) {
+        String name = ManagementFactory.getRuntimeMXBean().getName();
+        System.out.println(name);
+        // get pid
+        String pid = name.split("@")[0];
+        System.out.println("Pid is:" + pid);
         HashSet<String> hashSet = new HashSet<>();
         for (int i = 0; i < 100; i++) {
             hashSet.add(String.valueOf(i));
