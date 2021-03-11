115c107b2a3e39d5a644c64444deff2b3456e11c
semye <464622832@qq.com>
2021-03-11 16:33:23 +0800

    commit

--- src/main/java/com/semye/base/lang/ClassLoaderTest.java
+++ src/main/java/com/semye/base/lang/ClassLoaderTest.java
@@ -1,13 +1,33 @@
 package com.semye.base.lang;
 
+import java.net.URL;
+
 /**
  * Created by yesheng on 2021/3/10
  */
 public class ClassLoaderTest {
 
     public static void main(String[] args) {
-        System.out.println(ClassLoaderTest.class.getClassLoader().toString());
+
+        URL[] urLs = sun.misc.Launcher.getBootstrapClassPath().getURLs();
+        for (URL url : urLs) {
+            System.out.println(url.toExternalForm());
+        }
+
+        // java提供的类的classloader
+        ClassLoader classLoader = String.class.getClassLoader();
+        System.out.println(classLoader);
+
+        // extclassloader
+        String extDirs = System.getProperty("java.ext.dirs");
+        for (String path : extDirs.split(":")) {
+            System.out.println(path);
+        }
+
         Foo foo = new Foo();
+        // AppClassLoader
         System.out.println(foo.getClass().getClassLoader().toString());
+        // ExtClassLoader
+        System.out.println(foo.getClass().getClassLoader().getParent().toString());
     }
 }
