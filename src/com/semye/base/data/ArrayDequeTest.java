8dae2299e2dee0edde79f8f40c3eefbf33d92f84
semye <464622832@qq.com>
2022-11-05 22:29:42 +0800

    commit

--- src/com/semye/base/data/ArrayDequeTest.java
+++ src/com/semye/base/data/ArrayDequeTest.java
@@ -10,9 +10,20 @@ public class ArrayDequeTest {
     public static void main(String[] args) {
 
         ArrayDeque<String> arrayDeque = new ArrayDeque<>();
+        arrayDeque.add("1");
+        arrayDeque.add("2");
+        arrayDeque.add("3");
+        arrayDeque.add("4");
         arrayDeque.addFirst("yesheng");
         arrayDeque.addLast("ye");
-        System.out.println(arrayDeque.toString());
+        arrayDeque.offer("1");
+        arrayDeque.offer("99");
+        System.out.println(arrayDeque);
+        String el=arrayDeque.peek();
+        System.out.println(el);
+        System.out.println(arrayDeque);
+        arrayDeque.pop();
+        System.out.println(arrayDeque);
 
     }
 }
